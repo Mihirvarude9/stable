d0752d62e9fc11a8c5c886b14f6e4da4b2b68634
Mihirvarude9 <mvarude09@gmail.com>
2025-04-15 15:57:02 +0530

    new4

--- app.py
+++ app.py
@@ -131,4 +131,5 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="185.215.21.52", port=7861)
+    uvicorn.run(app, host="0.0.0.0", port=7861)
+
